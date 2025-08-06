7a74be8ed0abfec97988a4ae3addc49b2739ee23
Swikriti Chourasia <swikriti.333@gmail.com>
2025-08-06 21:04:44 +0200

    Added @beforeAll and implemented authentication under @Before
    read properties from property file

--- src/test/java/com/booking/stepdefinitions/Hooks.java
+++ src/test/java/com/booking/stepdefinitions/Hooks.java
@@ -1,11 +1,22 @@
-package com.booking.stepdefinitions;
+package com.booking.stepDefinitions;
 
+import com.POJO.AuthAPIReq;
+import com.booking.utils.APIResources;
+import com.booking.utils.JsonLoggingFilter;
+import com.booking.utils.ReadProperties;
 import com.booking.utils.TestContext;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.java.Scenario;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,13 +27,39 @@ public class Hooks {
         this.context = context;
     }
 
-    @Before
+    @BeforeAll
+    public static void before_or_after_all() {
+        ReadProperties.PropertyReader();
+    }
+
+    @Before(value = "not @noAuth", order = 0)
+    public void beforeScenario(Scenario scenario) {
+        RestAssured.reset();
+        AuthAPIReq authAPIReq = new AuthAPIReq();
+        authAPIReq.setUsername(ReadProperties.getValue("user.username"));
+        authAPIReq.setPassword(ReadProperties.getValue("user.password"));
+
+        APIResources resourceAPI = APIResources.valueOf("GetAuthTokenAPI");
+
+        RequestSpecification request = new RequestSpecBuilder().setBaseUri(ReadProperties.getValue("application.baseURI")).
+                setContentType(ContentType.JSON).build();
+        RequestSpecification res = given().spec(request).body(authAPIReq);
+        ResponseSpecification responseSpecification = new ResponseSpecBuilder().
+                expectStatusCode(200).expectContentType(ContentType.JSON).build();
+        Response response = res.when().post(resourceAPI.getResource())
+                .then().spec(responseSpecification).extract().response();
+
+        String token = response.path("token");
+        context.setSessionContext("token", token);
+    }
+
+    @Before(order = 1)
     public void setup() {
         RestAssured.reset();
         RequestSpecification request = new RequestSpecBuilder()
-                .setBaseUri("https://automationintesting.online")
+                .setBaseUri(ReadProperties.getValue("application.baseURI"))
                 .setContentType(ContentType.JSON)
-                .setAccept("application/json")
+                .setAccept(ReadProperties.getValue("accept"))
                 .build();
 
         context.requestSpec = given().spec(request);
