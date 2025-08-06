61f4bc9dd8efe1fa982767eca0dec6bf631e3698
Swikriti Chourasia <swikriti.333@gmail.com>
2025-08-06 15:28:11 +0200

    added a method createBookingRequestBody() to get json body for create booking api request

--- src/test/java/com/booking/stepdefinitions/BookingStepDefinitions.java
+++ src/test/java/com/booking/stepdefinitions/BookingStepDefinitions.java
@@ -1,13 +1,32 @@
 package com.booking.stepdefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.json.JSONObject;
 
 public class BookingStepDefinitions {
     @Given("User wants to do a booking with below booking details")
     public void userWantsToDoABookingWithBelowBookingDetails() {
+    private JSONObject createBookingRequestBody(Map<String, String> row, int roomid) {
+        int bookingid = 456;
+        return new JSONObject()
+                .put("bookingid", bookingid)
+                .put("roomid", roomid)
+                .put("firstname", safeValue(row.get("firstname")))
+                .put("lastname", safeValue(row.get("lastname")))
+                .put("depositpaid", true)
+                .put("email", safeValue(row.get("email")))
+                .put("phone", safeValue(row.get("phone")))
+                .put("bookingdates", new JSONObject()
+                        .put("checkin", safeValue(row.get("checkin")))
+                        .put("checkout", safeValue(row.get("checkout"))));
+    }
+
+    private String safeValue(String value) {
+        return value == null ? "" : value;
     }
 
     @When("User sends a POST request to {string} with the booking details")
