40f7b509ec028edef244031da2f6ed93094cfa64
Swikriti Chourasia <swikriti.333@gmail.com>
2025-08-06 16:02:10 +0200

    Created a TestContext file to store session context

---
+++ src/test/java/com/booking/utils/TestContext.java
@@ -0,0 +1,13 @@
+package com.booking.utils;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestContext {
+    public RequestSpecification requestSpec;
+    public Response response;
+    public Map<String, Object> session = new HashMap<>();
+}
