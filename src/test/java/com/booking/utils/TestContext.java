bfde32592b3a755398966f27e4024d3fef7534b6
Swikriti Chourasia <swikriti.333@gmail.com>
2025-08-06 19:13:58 +0200

    TestContext: added getter & settor

--- src/test/java/com/booking/utils/TestContext.java
+++ src/test/java/com/booking/utils/TestContext.java
@@ -5,15 +5,25 @@ import io.restassured.specification.RequestSpecification;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class TestContext {
     public RequestSpecification requestSpec;
     public Response response;
-    public Map<String, Object> session = new HashMap<>();
+    public Map<String, String> session = new HashMap<>();
     public Response getResponse() {
         return response;
     }
     public void setResponse(Response response) {
         this.response = response;
     }
+    public void setSessionContext(String key, String value) {
+        session.put(key, value);
+    }
+    public String getSessionContext(String key) {
+        return Optional.ofNullable((String) session.get(key))
+                .orElseThrow(() -> new NoSuchElementException("Key '" + key + "' not found in TestContext!"));
+    }
+
 }
