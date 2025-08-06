3524e3312df03f4cb8efaa5f03afe7907e50a084
Swikriti Chourasia <swikriti.333@gmail.com>
2025-08-06 13:27:05 +0200

    added FILTER_TAGS_PROPERTY_NAME in test runner file with value @booking_regression

--- src/test/java/com/booking/TestRunner.java
+++ src/test/java/com/booking/TestRunner.java
@@ -1,8 +1,8 @@
 package com.booking;
 
 import org.junit.platform.suite.api.*;
-import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+import static io.cucumber.core.options.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -10,5 +10,6 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.booking")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html, json:target/cucumber.json")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@booking_regression")
 public class TestRunner {
 }
